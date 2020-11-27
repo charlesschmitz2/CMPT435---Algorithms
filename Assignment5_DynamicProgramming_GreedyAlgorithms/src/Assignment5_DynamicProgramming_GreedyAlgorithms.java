6a25db3ed866b9780bad834e7bca45ec3d76443f
charlesschmitz2 <charlieschmitz23@gmail.com>
2020-11-27 17:22:52 -0500

    Testing Bellman Ford
    Works when hardcoded in, need to work functionality into loop and within graphs in file. Testing good so far though

--- Assignment5_DynamicProgramming_GreedyAlgorithms/src/Assignment5_DynamicProgramming_GreedyAlgorithms.java
+++ Assignment5_DynamicProgramming_GreedyAlgorithms/src/Assignment5_DynamicProgramming_GreedyAlgorithms.java
@@ -6,6 +6,51 @@ public class Assignment5_DynamicProgramming_GreedyAlgorithms {
     public static void main(String[] args) {
 
         readAndProcess("graphs2.txt");
+        int V = 5; // Number of vertices in graph
+        int E = 8; // Number of edges in graph
+
+        Graph graph = new Graph(V, E);
+
+        // add edge 0-1 (or A-B in above figure)
+        graph.edgeArray[0].source = 0;
+        graph.edgeArray[0].destination = 1;
+        graph.edgeArray[0].weight = -1;
+        // add edge 0-2 (or A-C in above figure)
+        graph.edgeArray[1].source = 0;
+        graph.edgeArray[1].destination = 2;
+        graph.edgeArray[1].weight = 4;
+
+        // add edge 1-2 (or B-C in above figure)
+        graph.edgeArray[2].source = 1;
+        graph.edgeArray[2].destination = 2;
+        graph.edgeArray[2].weight = 3;
+
+        // add edge 1-3 (or B-D in above figure)
+        graph.edgeArray[3].source = 1;
+        graph.edgeArray[3].destination = 3;
+        graph.edgeArray[3].weight = 2;
+
+        // add edge 1-4 (or A-E in above figure)
+        graph.edgeArray[4].source = 1;
+        graph.edgeArray[4].destination = 4;
+        graph.edgeArray[4].weight = 2;
+
+        // add edge 3-2 (or D-C in above figure)
+        graph.edgeArray[5].source = 3;
+        graph.edgeArray[5].destination = 2;
+        graph.edgeArray[5].weight = 5;
+
+        // add edge 3-1 (or D-B in above figure)
+        graph.edgeArray[6].source = 3;
+        graph.edgeArray[6].destination = 1;
+        graph.edgeArray[6].weight = 1;
+
+        // add edge 4-3 (or E-D in above figure)
+        graph.edgeArray[7].source = 4;
+        graph.edgeArray[7].destination = 3;
+        graph.edgeArray[7].weight = -3;
+
+        graph.bellmanFord(graph, 0);
 
     }//main
 
--- Assignment5_DynamicProgramming_GreedyAlgorithms/src/Graph.java
+++ Assignment5_DynamicProgramming_GreedyAlgorithms/src/Graph.java
@@ -18,11 +18,50 @@ public class Graph {
     public void bellmanFord(Graph graph, int source){
         int vertices = graph.vertices;
         int edges = graph.edges;
+        int distance[] = new int[vertices];
+
+        //1.
+        for (int i = 0; i < vertices; i++){
+            distance[i] = Integer.MAX_VALUE;
+        }//for
+        distance[source] = 0;
+
+        //2.
+        for (int i = 1; i < vertices; i++){
+            for (int j = 0; j < edges; j++){
+                int src = graph.edgeArray[j].source;
+                int dest = graph.edgeArray[j].destination;
+                int weight = graph.edgeArray[j].weight;
+
+                if ((distance[src] != Integer.MAX_VALUE) && (distance[src]+weight < distance[dest])){
+                    distance[dest] = distance[src] + weight;
+                }//if
+            }//for
+        }//for
+
+        //3.
+        for (int j = 0; j < edges; j++){
+            int src = graph.edgeArray[j].source;
+            int dest = graph.edgeArray[j].destination;
+            int weight = graph.edgeArray[j].weight;
+
+            if (distance[src] != Integer.MAX_VALUE && distance[src]+weight < distance[dest]){
+                System.out.println("There is a negative weight cycle within the graph");
+                return;
+            }//if
+        }//for
+
+        printSolution(distance, vertices);
+    }//BellmanFord
+
+    public void printSolution(int[] distance, int vertices) {
+        System.out.println("Vertex Distance from Source");
 
         for (int i = 0; i < vertices; i++){
-            
-        }
-    }
+            System.out.println(i + "\t\t" + distance[i]);
+
+        }//for
+    }//printSolution
 
 
 }//graph
