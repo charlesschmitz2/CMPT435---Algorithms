eca48e9995d50b7d2cb18e46f3d64b05e46f7e59
charlesschmitz2 <charlieschmitz23@gmail.com>
2020-12-04 23:36:58 -0500

    Still trying to get test function to work properly
    struggling with splitting into groups

--- Algorithms---SemesterProject/src/SemesterProjectMain.java
+++ Algorithms---SemesterProject/src/SemesterProjectMain.java
@@ -73,26 +73,32 @@ public class SemesterProjectMain {
 
     public static List<Person> people = new ArrayList<>();
     public static ListPeople peopleList = new ListPeople(people);
+    
+/*----Main----*/
     public static void main(String[] args) {
         
         //Run the simulation until you quit
         do{
             System.out.println("\n-------------RUNNING SIMULATION--------------");
+            
         } while(runSimulation());
         
             
 
-        }// main
+    }// main
 
+/*----Simulation Functions----*/
         //This is the function where the simulation is run and analized 
         public static boolean runSimulation(){
 
             menuSelection = menu();
+            infectedCounter = 0;
 
             if (menuSelection == 1){
                 System.out.println("\n----Running Simulation with 100 People---- \n");
                 peopleList.addPeople(100); //each time an addPeople is run the list is cleared and refilled so the program can run multiple cycles of differnet simulation sizes
                 runInfection();
+                test();
                 
                 //peopleList.print();
 
@@ -101,6 +107,7 @@ public class SemesterProjectMain {
                 System.out.println("\n----Running Simulation with 1,000 People---- \n");
                 peopleList.addPeople(1000);
                 runInfection();
+                test();
                 // peopleList.print();
 
             } // if 1000
@@ -108,6 +115,7 @@ public class SemesterProjectMain {
                 System.out.println("\n----Running Simulation with 10,000 People----\n");
                 peopleList.addPeople(10000);
                 runInfection();
+                test();
                 // peopleList.print();
 
             } // if 10000
@@ -115,6 +123,7 @@ public class SemesterProjectMain {
                 System.out.println("\n----Running Simulation with 100,000 People----\n");
                 peopleList.addPeople(100000);
                 runInfection();
+                test();
                 // peopleList.print();
 
             } // if 1000000
@@ -122,6 +131,7 @@ public class SemesterProjectMain {
                 System.out.println("\n----Running Simulation with 1,000,000 People----\n");
                 peopleList.addPeople(1000000);
                 runInfection();
+                test();
                 // peopleList.print();
 
             } // if 10000000
@@ -134,43 +144,19 @@ public class SemesterProjectMain {
 
         }//runSimulation
 
-        public static int menu() {
-
-            int selection;
-            Scanner input = new Scanner(System.in);
-
-            /***************************************************/
-
-            System.out.println("\nChoose the AMOUNT OF PEOPLE the Simulation will be Run With :");
-            System.out.println("1 - 100");
-            System.out.println("2 - 1,000");
-            System.out.println("3 - 10,000");
-            System.out.println("4 - 100,000");
-            System.out.println("5 - 1,000,000");
-            System.out.println("6 - Quit");
-
-            while (!input.hasNextInt()) {
-                String scanner = input.next();
-                System.out.print(" '" + scanner + "' is not a valid number.\n");
-            } // while
-
-            selection = input.nextInt();
-            if(selection == 6){input.close();}
-            
-            return selection;
-        }// menu
-
+/*----Menu Functions----*/
     public static void runInfection() {
         peopleList.diseaseGiver(infectionRate);//this is the function that infects people at the infection rate that is set (2% for us)
                 for(int i = 0; i < peopleList.size(); i++){
                     if (people.get(i).getIsSick()==1){
-                        if(peopleList.size()<=100000){System.out.println("\t\u2022 Person " + i + " has been infected");}
+                        if(peopleList.size()<=10000){System.out.println("\t\u2022 Person " + i + " has been infected");}
                         infectedCounter++;
                     }//if
                     
                 }//for
                 
-                if(peopleList.size()>100000){System.out.println("1,000,000 is too many to print out around 20,000 'has been infected' messages");}
+                if(peopleList.size()==1000000){System.out.println("1,000,000 is way too many to print out around 20,000 'has been infected' messages");}
+                if(peopleList.size()==100000){System.out.println("100,000 is too many to print out around 2,000 'has been infected' messages");}
 
                 if (infectedCounter == 0){
                     System.out.println("\tNo one was infected, unlikely but always possible...expect the unexpected");
@@ -187,7 +173,58 @@ public class SemesterProjectMain {
         //test each one of those 4 people and we tally up the total amount of tests needed to get through the entire group 
         //NOTE: while we may know which person is sick this is a simulation so we have to abide by these rules to get an accurate representation 
         //of how many tests are needed as there is no magic person node that marks people as sick or not sick that you can just summon up.
-    }
+
+        double numberTestGroups = Math.ceil((float) peopleList.size()/8); 
+        List<Person> tempTestArray = new ArrayList<>();
+       
+        int counter = 0;
+        int groupCounter = 0;
+        
+        while((numberTestGroups >= 0) && (counter < peopleList.size()+1)){
+            System.out.println(numberTestGroups + " >= 0");
+            int temp = peopleList.size()+1;
+            System.out.println(counter + " < " + temp);
+             if(tempTestArray.size() < 8){
+                 System.out.println("Adding to test group " + numberTestGroups + " item number " + groupCounter);
+                 groupCounter++;
+                 counter++;
+                 tempTestArray.add(people.get(counter));                 
+             }//if
+             else if (tempTestArray.size() == 8){
+                 System.out.println(tempTestArray);
+                 tempTestArray.clear();
+                 numberTestGroups--;
+             }
+        }//if        
+        
+
+    }//test
+
+    public static int menu() {
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        /***************************************************/
+
+        System.out.println("\nChoose the AMOUNT OF PEOPLE the Simulation will be Run With :");
+        System.out.println("1 - 100");
+        System.out.println("2 - 1,000");
+        System.out.println("3 - 10,000");
+        System.out.println("4 - 100,000");
+        System.out.println("5 - 1,000,000");
+        System.out.println("6 - Quit");
+
+        while (!input.hasNextInt()) {
+            String scanner = input.next();
+            System.out.print(" '" + scanner + "' is not a valid number.\n");
+        } // while
+
+        selection = input.nextInt();
+        if(selection == 6){input.close();}
+        
+        return selection;
+    }// menu
 
     //if time allows can adjust to work with different sized groups other than 8 may come back and implement later
     public static int menu2() {
@@ -197,8 +234,7 @@ public class SemesterProjectMain {
 
         /***************************************************/
 
-        System.out.println("Choose the GROUP SIZE the Simulation will be Run With :");
-        System.out.println("-------------------------\n");
+        System.out.println("\nChoose the GROUP SIZE the Simulation will be Run With :");
         System.out.println("1 - 4 Groups");
         System.out.println("2 - 6 Groups");
         System.out.println("3 - 8 Groups");
